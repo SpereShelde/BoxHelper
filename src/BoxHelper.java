e48ce26e37bd445350d8d551222ba01fc97ba4c0
SpereShelde <spereshelde@gmail.com>
2018-06-22 10:59:49 +0800

    Test disk limit

--- src/BoxHelper.java
+++ src/BoxHelper.java
@@ -4,7 +4,9 @@ import org.openqa.selenium.Cookie;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import tools.ConvertJson;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.*;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -64,6 +66,65 @@ public class BoxHelper {
         System.out.println("Initialization done.");
     }
 
+    private String getMaxDisk(){
+
+        String maxDisk = "/home";
+        try {
+            Runtime runtime = Runtime.getRuntime();
+            Process process = runtime.exec("sh -c df -l | awk '{print $4, $5, $6}'");
+            BufferedReader in = null;
+            try {
+                in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                String maxLine = null;
+                int maxSize = 0;
+                int currentSize = 0;
+                String line = in.readLine();
+                while ((line = in.readLine()) != null) {
+                    currentSize = Integer.parseInt(line.substring(0, line.indexOf(" ")));
+                    if (currentSize > maxSize){
+                        maxSize = currentSize;
+                        maxLine = line;
+                    }
+                }
+                maxDisk = maxLine.substring(maxLine.lastIndexOf(" ") + 1);
+            } catch (Exception e) {
+                System.out.println("Cannot get max disk 1.");
+            } finally {
+                in.close();
+                System.exit(107);
+            }
+        } catch (Exception e) {
+            System.out.println("Cannot get max disk 2.");
+            System.exit(108);
+        }
+        return maxDisk;
+    }
+
+    private boolean canContinue(String disk, int limit){
+        boolean flag = true;
+
+        try {
+            Runtime runtime = Runtime.getRuntime();
+            Process process = runtime.exec("sh -c df -l | grep " + disk + " | awk '{print $5, $6}'");
+            BufferedReader in = null;
+            try {
+                in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                String line = in.readLine();
+                if (Integer.parseInt(line.substring(0, line.indexOf("%"))) < limit) flag = false;
+            } catch (Exception e) {
+                System.out.println("Cannot restrict 1.");
+            } finally {
+                in.close();
+                System.exit(109);
+            }
+        } catch (Exception e) {
+            System.out.println("Cannot restrict 2.");
+            System.exit(110);
+        }
+
+        return flag;
+    }
+
     public static void main(String[] args) {
 
 
@@ -76,6 +137,11 @@ public class BoxHelper {
         if ("true".equals(boxHelper.configures.get("isSticky").toString())) {
             type += 2;
         }
+        String maxDisk = "";
+        int limit = Integer.getInteger(boxHelper.configures.get("diskLimit").toString());
+        if (limit != -1 && limit != 0) {
+            maxDisk = boxHelper.getMaxDisk();
+        }
         int cpuThreads = Runtime.getRuntime().availableProcessors();
         int count  = 1;
         ArrayList<Spider> spiders = new ArrayList<>();
@@ -84,6 +150,12 @@ public class BoxHelper {
             spiders.add(new Spider(url.substring(url.indexOf("//") + 2, url.indexOf("/", 8)), url, boxHelper.configures.get("path").toString(), type, Double.parseDouble(boxHelper.configures.get("min").toString()), Double.parseDouble(boxHelper.configures.get("max").toString()), boxHelper.driver));
         }
         while (true){
+            if (limit != -1 && limit != 0) {
+                if (!boxHelper.canContinue(maxDisk, limit)){
+                    System.out.println("Reached limit, exit.");
+                    System.exit(111);
+                }
+            }
             ExecutorService executorService = Executors.newFixedThreadPool(cpuThreads);
             System.out.println("\nBoxHelper " + count + " begins at " + time());
 
